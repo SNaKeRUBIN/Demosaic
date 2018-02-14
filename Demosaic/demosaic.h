8805e18f6184b80612e729ff67ed07e8ec1934f7
SNaKeRUBIN <pannu.abhinoor@gmail.com>
2018-02-14 14:27:36 -0500

    Fixed the indentation in header files

--- Demosaic/demosaic.h
+++ Demosaic/demosaic.h
@@ -3,9 +3,9 @@
 
 #include "opencv2\opencv.hpp"
 
-	cv::Mat demosaic_freeman(const cv::Mat b, const cv::Mat g, const cv::Mat r);
+cv::Mat demosaic_freeman(const cv::Mat b, const cv::Mat g, const cv::Mat r);
 
-	cv::Mat demosaic_bayer(const cv::Mat b, const cv::Mat g, const cv::Mat r);
+cv::Mat demosaic_bayer(const cv::Mat b, const cv::Mat g, const cv::Mat r);
 
 
 #endif //DEMOSAIC_H
--- Demosaic/utils.h
+++ Demosaic/utils.h
@@ -3,8 +3,8 @@
 
 #include "opencv2\opencv.hpp"
 
-	cv::Mat calc_diff(const cv::Mat original, const cv::Mat colored);
+cv::Mat calc_diff(const cv::Mat original, const cv::Mat colored);
 
-	cv::Mat getMask(cv::Size size, char channel);
+cv::Mat getMask(cv::Size size, char channel);
 
 #endif //UTILS_H
